52331479c8fcdf3890a4146a6705557622aaffdd
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2022-12-06 06:12:09 +0100

    day6

---
+++ 2022/day6/day6_python/day6_python_1.py
@@ -0,0 +1,17 @@
+
+from pathlib import Path
+CURR_DIR = Path()
+
+day_input_file = open(CURR_DIR / 'day6_input.txt')
+day_input = day_input_file.readlines()
+# day_input = day_input_file.read()
+
+stack = []
+for idx, char in enumerate(day_input[0]):
+    stack.append(char)
+    if len(stack) == 4:
+        if len(set(stack)) == 4:
+            print(idx+1, print(stack))
+            break
+        else:
+            stack.pop(0)
