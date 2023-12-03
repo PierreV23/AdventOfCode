0d7d1a5dd0c8d5c201ea731a8831502318c245a3
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-12-03 14:54:51 +0100

    updated templates

--- 2023/template/part1.py
+++ 2023/template/part1.py
@@ -1,9 +1,13 @@
+from typing import Iterable
+
+
 def parser(file = 'input.txt'):
     with open(file, 'r') as file:
-        return file.readlines()
+        for line in file:
+            yield line.strip('\n')
 
-def solver(lines: list[str]):
-    ...
+def solver(lines: Iterable[str]):
+    lines = [*lines]
 
 
 if __name__ == '__main__':
