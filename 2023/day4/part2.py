f5ab999580dd462cd77e5cea7dc444f972874e35
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2023-12-04 11:16:52 +0100

    day4part2

--- 2023/day4/part2.py
+++ 2023/day4/part2.py
@@ -1,4 +1,5 @@
 from typing import Iterable
+from collections import defaultdict
 
 
 def parser(file = 'input.txt'):
@@ -8,10 +9,29 @@ def parser(file = 'input.txt'):
 
 def solver(lines: Iterable[str]):
     lines = [*lines]
+    summed = 0
+    stack = defaultdict(int)
+    highest = int(lines[-1].split(": ")[0].split(' ')[1])
+    print(highest)
+    for line in lines:
+        card_id, cards = line.split(": ")
+        card_id = int(card_id.split(' ')[-1])
+        wc, mc = cards.split(" | ")
+        wc, mc = wc.split(' '), mc.split(' ')
+        wc, mc = [c for c in wc if c], [c for c in mc if c]
+        count = sum([c in mc for c in wc])
+        print(count)
+        stack[card_id] += 1
+        if any(c in mc for c in wc):
+            for n in range(card_id+1, card_id+count+1):
+                if n <= highest:
+                    stack[n] += max(stack[card_id], 1)
+        print(stack)
+    return sum(stack.values())
 
 
 if __name__ == '__main__':
-    test = solver(parser('test_input.txt'))
+    test = solver(parser('test_input_p2.txt'))
     print(test)
-    assert test == None
+    assert test == 30
     print(solver(parser()))
