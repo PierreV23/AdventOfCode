3e22da99bb0d84dbd148093897f6a7fce3680854
PierreV23 <77776198+PierreV23@users.noreply.github.com>
2023-12-04 10:55:26 +0100

    day4part1

--- 2023/day4/part1.py
+++ 2023/day4/part1.py
@@ -8,10 +8,33 @@ def parser(file = 'input.txt'):
 
 def solver(lines: Iterable[str]):
     lines = [*lines]
+    summed = 0
+    for line in lines:
+        _card_id, cards = line.split(": ")
+        wc, mc = cards.split(" | ")
+        wc, mc = wc.split(' '), mc.split(' ')
+        wc, mc = [c for c in wc if c], [c for c in mc if c]
+        count = 0
+        for idx, c in enumerate(wc):
+            # if c in mc:
+            #     if idx > 0:
+            #         if w[idx-1]:
+            #             w.append(w[idx-1]*2)
+            #             w[idx-1] = 0
+            #         else:
+            #             w.append(1) 
+            #     else:
+            #         w.append(1)
+            # else:
+            #     w.append(0)
+            count += c in mc
+        print(2**(count-1) if count > 0 else 0)
+        summed += 2**(count-1) if count > 0 else 0
+    return summed
 
 
 if __name__ == '__main__':
     test = solver(parser('test_input.txt'))
     print(test)
-    assert test == None
+    assert test == 13
     print(solver(parser()))
