9e5c2042a41e90977427813cf1be9a1000fc862d
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-12-08 10:08:47 +0100

    day8part1

--- 2023/day8/part2.py
+++ 2023/day8/part2.py
@@ -1,17 +1,31 @@
+from itertools import cycle
 from typing import Iterable
 
 
 def parser(file = 'input.txt'):
     with open(file, 'r') as file:
-        for line in file:
-            yield line.strip('\n')
+        path, link = file.read().strip().split("\n\n")
+        p = lambda s: s.strip('(').strip(')').split(", ")
+        links = {l.split(' = ')[0]:l.split(' = ')[1].strip('(').strip(')').split(", ") for l in link.split('\n')}
+        return path, links
 
-def solver(lines: Iterable[str]):
-    lines = [*lines]
+def solver(path, links):
+    pos = "AAA"
+    for counter, step in enumerate(cycle(path)):
+        # print(counter, step, pos)
+        if pos == "ZZZ":
+            break
+        pos = links[pos]["LR".index(step)]
+    else:
+        print("nuh uh")
+    return counter
 
 
 if __name__ == '__main__':
-    test = solver(parser('test_input.txt'))
+    test = solver(*parser('test_input.txt'))
     print(test)
-    assert test == None
-    print(solver(parser()))
+    assert test == 2
+    test = solver(*parser('test_input_2.txt'))
+    print(test)
+    assert test == 6
+    print(solver(*parser()))
