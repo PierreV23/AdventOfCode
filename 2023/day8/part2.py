c864f6ad8e11330b330bb6de1667efe3b988e2b1
Pièrre <77776198+PierreV23@users.noreply.github.com>
2023-12-08 10:29:03 +0100

    day8part2

--- 2023/day8/part2.py
+++ 2023/day8/part2.py
@@ -1,7 +1,6 @@
 from itertools import cycle
 from typing import Iterable
 
-
 def parser(file = 'input.txt'):
     with open(file, 'r') as file:
         path, link = file.read().strip().split("\n\n")
@@ -10,22 +9,38 @@ def parser(file = 'input.txt'):
         return path, links
 
 def solver(path, links):
-    pos = "AAA"
+    finished = []
+    pos = [l for l in links.keys() if l[-1] == 'A']
     for counter, step in enumerate(cycle(path)):
         # print(counter, step, pos)
-        if pos == "ZZZ":
+        if not pos:
             break
-        pos = links[pos]["LR".index(step)]
+        # print(len([p for p in pos if p[-1] =='Z']), len(pos), pos)
+        c = pos[::]
+        pos = []
+        for p in c:
+            if p[-1] == 'Z':
+                finished.append(counter)
+            else:
+                pos.append(links[p]["LR".index(step)])
     else:
         print("nuh uh")
-    return counter
+    print(finished, counter)
+    return finished
+
+
+def find_lcm(a):
+    # credit
+    # https://stackoverflow.com/a/42472824
+    from math import gcd
+    lcm = 1
+    for i in a:
+        lcm = lcm*i//gcd(lcm, i)
+    return lcm
 
 
 if __name__ == '__main__':
-    test = solver(*parser('test_input.txt'))
-    print(test)
-    assert test == 2
-    test = solver(*parser('test_input_2.txt'))
+    test = find_lcm(solver(*parser('test_input_p2.txt')))
     print(test)
     assert test == 6
-    print(solver(*parser()))
+    print(find_lcm(solver(*parser())))
