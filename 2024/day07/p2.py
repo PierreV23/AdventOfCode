407015ef49ef220cf14797e7d6ce96bdf9549277
Pièrre <77776198+PierreV23@users.noreply.github.com>
2024-12-13 01:14:05 +0100

    small refactor of 2024 day07 p2

--- 2024/day07/p2.py
+++ 2024/day07/p2.py
@@ -15,21 +15,20 @@ with open(sys.argv[1], 'r') as file:
 
 def rec(ns: list[int], res: int):
     merge = lambda a, b: a * 10**int(log10(b) + 1) + b
-    if len(ns) == 2:
-        a, b = ns
-        if a + b == res or a * b == res or merge(a, b) == res:
-            return True
-    else:
-        for op in (
-            int.__mul__,
-            int.__add__,
-            merge
-        ):
-            ls = ns[1:]
-            ls[0] = op(ns[0], ls[0])
-            r = rec(ls, res)
-            if r:
+    for op in (
+        int.__mul__,
+        int.__add__,
+        merge
+    ):
+        ls = ns[1:]
+        ls[0] = op(ns[0], ls[0])
+        if len(ls) == 1:
+            if ls[0] == res:
                 return True
+            continue
+        r = rec(ls, res)
+        if r:
+            return True
     return False
 
 s = 0
